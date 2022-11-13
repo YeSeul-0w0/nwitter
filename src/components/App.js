afe98db449a1b5f1fa7869c552d841b732a8d404
YeSeul-0w0 <choi333ys@gmail.com>
2022-11-13 17:43:00 +0900

    add auth function

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import AppRouter from "./Router";
+import {useEffect, useState} from "react";
+import { authService } from "../firebase";
 
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+      authService.onAuthStateChanged((user) => {
+          if (user) {
+              setIsLoggedIn(user);
+          } else {
+              setIsLoggedIn(false);
+          }
+          setInit(true);
+      })
+  }, [])
+
   return (
-    <AppRouter />
+      <>
+        {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+        <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
+      </>
   );
 }
 
