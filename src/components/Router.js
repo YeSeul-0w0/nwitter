db3a2e14f55ae5fb6486919496f57bcbb36832d1
YeSeul-0w0 <choi333ys@gmail.com>
2022-11-14 21:41:21 +0900

    add logout function

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn}) => {
     return (
         <BrowserRouter>
+          {isLoggedIn && <Navigation /> }
             <Routes>
                 {isLoggedIn ? (
-                    <Route path="/" element={<Home />}></Route>
+                  <>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />}/>
+                  </>
                 ): (
                     <Route path="/" element={<Auth />}></Route>
                 )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,20 @@
-const Profile = () => <span> Auth </span>
+import {authService} from "../firebase";
+import {useNavigate } from "react-router-dom";
+
+
+const Profile = () => {
+  const history = useNavigate ();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history("/");
+  }
+
+  return(
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  )
+}
 
 export default Profile;
