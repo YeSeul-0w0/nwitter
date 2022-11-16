64f168826e0a394cbd37bdd2eae4b957009f6243
YeSeul-0w0 <choi333ys@gmail.com>
2022-11-16 21:01:54 +0900

    create nweets function

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,14 +5,14 @@ import Auth from "../routes/Auth";
 import Profile from "../routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({ isLoggedIn}) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
     return (
         <BrowserRouter>
           {isLoggedIn && <Navigation /> }
             <Routes>
                 {isLoggedIn ? (
                   <>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home userObj={userObj} />} />
                     <Route path="/profile" element={<Profile />}/>
                   </>
                 ): (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,56 @@
-const Home = () => <span> Home </span>
+import {useEffect, useState} from "react";
+import {dbService} from "../firebase";
+import {addDoc, collection, onSnapshot} from "firebase/firestore"
+
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(()=>{
+    onSnapshot(collection(dbService, "nweets"), (snapshot => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    }));
+  }, [])
+
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createAt: Date.now(),
+      createdId: userObj.uid,
+    });
+    setNweet("");
+  }
+
+  const onChange = (event) => {
+    event.preventDefault();
+
+    const {
+      target: {value},
+    } = event;
+    setNweet(value);
+  }
+
+  return(
+    <>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
+        <input type="submit" value="Nweet"/>
+      </form>
+      <div>
+        {nweets.map((data) =>(
+          <div key={data.id}>
+            <h4>{data.text}</h4>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 
 export default Home;
