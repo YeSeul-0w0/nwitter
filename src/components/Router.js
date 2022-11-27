9beedb8ebd419b2a3e37c44aa289980552ae5ad4
YeSeul-0w0 <choi333ys@gmail.com>
2022-11-27 16:42:45 +0900

    light mode and apply style

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,34 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 import Profile from "../routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
+const AppRouter = ({isLoggedIn, userObj, refreshUser}) => {
     return (
         <BrowserRouter>
-          {isLoggedIn && <Navigation userObj={userObj}/> }
-            <Routes>
-                {isLoggedIn ? (
-                  <>
-                    <Route path="/nwitter" element={<Home userObj={userObj} />} />
-                    <Route path="/nwitter/profile" element={<Profile refreshUser={refreshUser} userObj={userObj} />}/>
-                  </>
-                ): (
-                    <Route path="/nwitter" element={<Auth />}></Route>
-                )}
-            </Routes>
+            {isLoggedIn && <Navigation userObj={userObj}/>}
+            <div style={{
+                maxWidth: 890,
+                width: "100%",
+                margin: "0 auto",
+                marginTop: 80,
+                display: "flex",
+                justifyContent: "center",
+            }}>
+                <Routes>
+                    {isLoggedIn ? (
+                        <>
+                            <Route path="/nwitter" element={<Home userObj={userObj}/>}/>
+                            <Route path="/nwitter/profile"
+                                   element={<Profile refreshUser={refreshUser} userObj={userObj}/>}/>
+                        </>
+                    ) : (
+                        <Route path="/nwitter" element={<Auth/>}></Route>
+                    )}
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 };
